e858dc605278f530dd743d3ccaffd7c97a513230
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-11-09 14:56:43 -0800

    Style header and header logo

--- CV-Builder-App/src/components/PersonalDetails.jsx
+++ CV-Builder-App/src/components/PersonalDetails.jsx
@@ -1,55 +1,61 @@
 export default function PersonalDetails({handleChange}) {
   return (
-    <div className="personal-details">
-      <h1>Personal Details</h1>
-      <label>
-        First Name
-        <input
-        type="text" 
-        name='firstName'
-        onChange={handleChange}
-        ></input>
-      </label>
-      <label>
-        Last Name
-        <input
-        type="text" 
-        name='lastName'
-        onChange={handleChange}
-        ></input>
-      </label>
-      <label>
-        Email
-        <input
-        type="email" 
-        name='email'
-        onChange={handleChange}
-        ></input>
-      </label>
-      <label>
-        Phone Number
-        <input
-        type="tel" 
-        name='phoneNumber'
-        onChange={handleChange}
-        ></input>
-      </label>
-      <label>
-        Country
-        <input
-        type="text" 
-        name='country'
-        onChange={handleChange}
-        ></input>
-      </label>
-      <label>
-        City
-        <input
-        type="text" 
-        name='city'
-        onChange={handleChange}
-        ></input>
-      </label>
-    </div>
+    <>
+      <div className="logo-text">
+        <img src="./logo.png" className="logo"/>
+        CV Craft
+      </div>
+      <div className="personal-details">
+        <h1>Personal Details</h1>
+        <label>
+          First Name
+          <input
+          type="text" 
+          name='firstName'
+          onChange={handleChange}
+          ></input>
+        </label>
+        <label>
+          Last Name
+          <input
+          type="text" 
+          name='lastName'
+          onChange={handleChange}
+          ></input>
+        </label>
+        <label>
+          Email
+          <input
+          type="email" 
+          name='email'
+          onChange={handleChange}
+          ></input>
+        </label>
+        <label>
+          Phone Number
+          <input
+          type="tel" 
+          name='phoneNumber'
+          onChange={handleChange}
+          ></input>
+        </label>
+        <label>
+          Country
+          <input
+          type="text" 
+          name='country'
+          onChange={handleChange}
+          ></input>
+        </label>
+        <label>
+          City
+          <input
+          type="text" 
+          name='city'
+          onChange={handleChange}
+          ></input>
+        </label>
+      </div>
+    </>
   )
 }
