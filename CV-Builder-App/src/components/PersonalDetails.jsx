4acbb3703bff559cf722d9a39e24a19c37bab1d3
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-11-09 14:10:25 -0800

    Create PersonalDetails component so user can input personal details

---
+++ CV-Builder-App/src/components/PersonalDetails.jsx
@@ -0,0 +1,14 @@
+export default function PersonalDetails() {
+  return (
+    <div className="personal-details">
+      <h1>Personal Details</h1>
+      <label>
+        First Name
+        <input
+        type="text" 
+        name='firstName'
+        ></input>
+      </label>
+    </div>
+  )
+}
