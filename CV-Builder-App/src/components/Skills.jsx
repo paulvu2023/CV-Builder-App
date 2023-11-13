5c9a85b678d7a2faead4dbd84ef47d75349a860b
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-11-13 15:35:23 -0800

    Update Skills component css

--- CV-Builder-App/src/components/Skills.jsx
+++ CV-Builder-App/src/components/Skills.jsx
@@ -5,13 +5,13 @@ export default function Skills({skillsData, addSkill, removeSkill}) {
       <label className="professional-summary-label">
         Choose important skills that show you fit the position. Make sure they match the key skills in the job listing.
       </label>
-      <form onSubmit={addSkill}>
+      <form onSubmit={addSkill} className="skills-form">
         <input type="text" id="skill"/>
         <button className="skill-button">Add Skill</button>
       </form>
       {skillsData.map((skill) => {
         return (
-          <div key={skill} className="skillItem">
+          <div key={skill} className="skill-item">
             <div>{skill}</div>
             <i onClick={() => {removeSkill(skill)}} className="fa-solid fa-trash"></i>
           </div>
