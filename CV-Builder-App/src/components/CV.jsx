3e9a4d07ec8bb2dd12fc3e6c853dce1217bbf91d
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-11-15 22:17:27 -0800

    Decrease size of CV and all of its children elements

--- CV-Builder-App/src/components/CV.jsx
+++ CV-Builder-App/src/components/CV.jsx
@@ -6,15 +6,15 @@ export default function CV({detailsData, educationData, employmentData, skillsDa
           <div className="subheader white">Contact</div>
           <div className="wrapper">
             <h2 className="white">Phone</h2>
-            <div className="white">{detailsData.phoneNumber || '012-345-6789'}</div>
+            <div className="white smaller">{detailsData.phoneNumber || '012-345-6789'}</div>
           </div>
           <div className="wrapper">
             <h2 className="white">Email</h2>
-            <div className="white">{detailsData.email || 'example@gmail.com'}</div>
+            <div className="white smaller">{detailsData.email || 'example@gmail.com'}</div>
           </div>
           <div className="wrapper">
             <h2 className="white">Location</h2>
-            <div className="white">
+            <div className="white smaller">
               {detailsData.city || detailsData.country
                 ? `${detailsData.city || ''}${detailsData.country && `, ${detailsData.country}`}`
                 : 'City, Country'}
@@ -24,25 +24,25 @@ export default function CV({detailsData, educationData, employmentData, skillsDa
           {educationData[0].school ? (
             educationData.map((educationItem, index) => (
               <div key={index} className="wrapper">
-                <div className="white">
+                <div className="white smaller">
                   {educationItem.startDate}
                   {educationItem.endDate && ` - ${educationItem.endDate}`}
                 </div>
                 <h2 className="white">{educationItem.degree}</h2>
-                <div className="white">{educationItem.school}</div>
+                <div className="white smaller">{educationItem.school}</div>
               </div>
             ))
           ) : (
             <>
               <div className="wrapper">
-                <div className="white">Start Date - End Date</div>
+                <div className="white smaller">Start Date - End Date</div>
                 <h2 className="white">Degree</h2>
-                <div className="white">School</div>
+                <div className="white smaller">School</div>
               </div>
               <div className="wrapper">
-                <div className="white">Start Date - End Date</div>
+                <div className="white smaller">Start Date - End Date</div>
                 <h2 className="white">Degree</h2>
-                <div className="white">School</div>
+                <div className="white smaller">School</div>
               </div>
             </>
           )}
@@ -55,12 +55,12 @@ export default function CV({detailsData, educationData, employmentData, skillsDa
               )
             }) : 
             <>
-              <li className="white">Skill 1</li>
-              <li className="white">Skill 2</li>
-              <li className="white">Skill 3 </li>
-              <li className="white">Skill 4</li>
-              <li className="white">Skill 5</li>
-              <li className="white">Skill 6 </li>
+              <li className="white smaller">Skill 1</li>
+              <li className="white smaller">Skill 2</li>
+              <li className="white smaller">Skill 3 </li>
+              <li className="white smaller">Skill 4</li>
+              <li className="white smaller">Skill 5</li>
+              <li className="white smaller">Skill 6 </li>
             </>
             }
           </ul>
@@ -84,9 +84,9 @@ export default function CV({detailsData, educationData, employmentData, skillsDa
           <>
             {[1, 2, 3].map((index) => (
               <div key={index} className="experience-item">
-                <div className="bold">Start - End</div>
-                <div>Company | Location</div>
-                <h5 className="bold">Job Title</h5>
+                <div className="bold smaller">Start - End</div>
+                <div className="smaller">Company | Location</div>
+                <h5 className="bold smaller">Job Title</h5>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis lectus ut orci congue imperdiet.</p>
               </div>
             ))}
